9aedede3197946d49692b063a033c68032f45792
mbilker <me@mbilker.us>
2015-12-16 15:25:34 -0500

    import custom dircolors

---
+++ zsh/.zsh/dircolors.zsh
@@ -0,0 +1,20 @@
+#
+#                 ██     
+#                ░██     
+#  ██████  ██████░██     
+# ░░░░██  ██░░░░ ░██████ 
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░ 
+#
+#  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░▓ about           ▓ custom linux config files
+# ░▓ author          ▓ mbilker <me@mbilker.us>
+# ░▓ original author ▓ xero <x@xero.nu>
+# ░▓ code            ▓ https://github.com/mbilker/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░░░░░░░░░
+#
+#█▓▒░ dircolors import
+eval `dircolors ~/.dircolors`
