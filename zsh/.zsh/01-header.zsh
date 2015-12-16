7a60755f7251602f77e33367d7659cc24502a14f
mbilker <me@mbilker.us>
2015-12-16 16:30:37 -0500

    add command line header when opening new terminal window

---
+++ zsh/.zsh/01-header.zsh
@@ -0,0 +1,24 @@
+#
+#                 ██     
+#                ░██     
+#  ██████  ██████░██     
+# ░░░░██  ██░░░░ ░██████ 
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░ 
+#
+#  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░▓ about           ▓ custom linux config files
+# ░▓ author          ▓ mbilker <me@mbilker.us>
+# ░▓ original author ▓ xero <x@xero.nu>
+# ░▓ code            ▓ https://github.com/mbilker/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░░░░░░░░░
+#
+#█▓▒░ display name in toilet font
+echo
+echo mbilker | toilet -f 3d | lolcat
+
+#█▓▒░ display random fortune
+fortune | cowsay -f small | lolcat
