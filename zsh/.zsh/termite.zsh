2827996dd92754b17177a16ca643b20bdde8776c
mbilker <me@mbilker.us>
2015-12-23 15:36:32 -0500

    Add VTE initialization

---
+++ zsh/.zsh/termite.zsh
@@ -0,0 +1,19 @@
+#
+#                 ██     
+#                ░██     
+#  ██████  ██████░██     
+# ░░░░██  ██░░░░ ░██████ 
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░ 
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ about  ▓ custom linux config files
+# ░▓ author ▓ mbilker <me@mbilker.us>
+# ░▓ code   ▓ https://github.com/mbilker/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+#
+#█▓▒░ import vte profile file for termite
+source /etc/profile.d/vte.sh
