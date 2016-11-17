2f4826a49da99162cda878714724bc12213f54e8
mbilker <me@mbilker.us>
2016-11-17 15:43:06 -0500

    zsh: 3DS devkitpro env variables

---
+++ zsh/.zsh/devkitpro.zsh
@@ -0,0 +1,21 @@
+#
+#                 ██     
+#                ░██     
+#  ██████  ██████░██     
+# ░░░░██  ██░░░░ ░██████ 
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░ 
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ about  ▓ custom linux config files
+# ░▓ author ▓ mbilker <me@mbilker.us>
+# ░▓ code   ▓ https://github.com/mbilker/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+#
+#█▓▒░ devkitpro environment variables
+export DEVKITPRO=/opt/devkitPro
+export DEVKITARM=$DEVKITPRO/devkitARM
+export PATH=$PATH:$DEVKITARM/bin
