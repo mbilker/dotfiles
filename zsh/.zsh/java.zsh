6cc3229b4a5e6f3c091cbd175f3541010499210f
mbilker <me@mbilker.us>
2017-02-08 04:55:23 -0500

    zsh: add Java antialiasing options

---
+++ zsh/.zsh/java.zsh
@@ -0,0 +1,19 @@
+#                 ██      
+#                ░██      
+#  ██████  ██████░██      
+# ░░░░██  ██░░░░ ░██████  
+#    ██  ░░█████ ░██░░░██ 
+#   ██    ░░░░░██░██  ░██ 
+#  ██████ ██████ ░██  ░██ 
+# ░░░░░░ ░░░░░░  ░░   ░░  
+#
+#  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░▓ about           ▓ custom linux config files
+# ░▓ author          ▓ mbilker <me@mbilker.us>
+# ░▓ original author ▓ xero <x@xero.nu>
+# ░▓ code            ▓ https://github.com/mbilker/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░░░░░░░░░░
+#
+#█▓▒░ better font rendering
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
