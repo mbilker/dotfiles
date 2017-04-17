48c67b91f2dd045c5f0d566bf19c0d09e8baf6f6
mbilker <me@mbilker.us>
2017-04-17 02:50:25 -0400

    scripts: switch to Optimus display (DP-4 -> eDP-1-1)

--- scripts/opt/scripts/dock_helper.sh
+++ scripts/opt/scripts/dock_helper.sh
@@ -5,7 +5,8 @@ FIRST_EXTERNAL_SETTING="nvidia-auto-select +0+0"
 FIRST_EXTERNAL_HORIZONTAL_SIZE="1920"
 SECOND_EXTERNAL="DP-3.1"
 SECOND_EXTERNAL_SETTING="nvidia-auto-select +${FIRST_EXTERNAL_HORIZONTAL_SIZE}+0"
-LCD_SCREEN="DP-4"
+#LCD_SCREEN="DP-4"
+LCD_SCREEN="eDP-1-1"
 
 TRACKPOINT="TPPS/2 IBM TrackPoint"
 TOUCHPAD="SynPS/2 Synaptics TouchPad"
@@ -23,8 +24,8 @@ TRACKPOINT_IDS=$(xinput --list | awk -v search="$TRACKPOINT" \
 case "$1" in
   "0")
     # undocked
-    /usr/bin/nvidia-settings -a CurrentMetaMode="${LCD_SCREEN}: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
-    /usr/bin/xrandr --output "${LCD_SCREEN}" --primary
+    #/usr/bin/nvidia-settings -a CurrentMetaMode="${LCD_SCREEN}: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+    /usr/bin/xrandr --output "${LCD_SCREEN}" --primary --auto --output "${FIRST_EXTERNAL}" --off --output "${SECOND_EXTERNAL}" --off
     for id in $TRACKPOINT_IDS; do
       /usr/bin/xinput set-int-prop "${id}" "Device Enabled" 8 1
     done
@@ -33,6 +34,7 @@ case "$1" in
     ;;
   "1")
     # docked
+    /usr/bin/xrandr --output "${LCD_SCREEN}" --off
     /usr/bin/nvidia-settings -a CurrentMetaMode="${FIRST_EXTERNAL}: ${FIRST_EXTERNAL_SETTING} { ForceFullCompositionPipeline = On }, ${SECOND_EXTERNAL}: ${SECOND_EXTERNAL_SETTING} { ForceFullCompositionPipeline = On }"
     /usr/bin/xrandr --output "${FIRST_EXTERNAL}" --primary
     for id in $TRACKPOINT_IDS; do
