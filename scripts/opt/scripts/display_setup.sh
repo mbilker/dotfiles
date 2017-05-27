f58f5e7fd243b44f5e19800714016670e0e0f1fa
mbilker <me@mbilker.us>
2017-05-26 23:32:06 -0400

    scripts: detect Intel HD GPU before doing hybrid graphics setup

--- scripts/opt/scripts/display_setup.sh
+++ scripts/opt/scripts/display_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Early exit if no Intel HD Graphics device present
+test -z "$(/usr/sbin/lspci | grep "HD Graphics")" && exit 0
+
 xrandr --setprovideroutputsource modesetting NVIDIA-0
 #xrandr --setprovideroutputsource Intel NVIDIA-0
 xrandr --dpi 96
