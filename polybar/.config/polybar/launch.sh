52ac75bbdb6ac1d2e2971e3783b760a723eb9860
mbilker <me@mbilker.us>
2017-07-23 18:01:22 -0400

    polybar: add more monitor configurations

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,9 +10,11 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 #polybar -q top &
 #MONITOR=DP-1 polybar -q bottom &
 #MONITOR=DP-3.1 polybar -q multimonitor &
+MONITOR=DP-1 polybar -q bottom &
 MONITOR=DP-3.1 polybar -q bottom &
 MONITOR=DP-3.2 polybar -q bottom &
 MONITOR=DP-3.3 polybar -q multimonitor &
+MONITOR=DP-4 polybar -q bottom &
 polybar -q bottom &
 
 echo "Bars launched..."
