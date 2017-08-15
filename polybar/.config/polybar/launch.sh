ea0dd174a0e3751da4818a87d744fa0d1df28884
mbilker <me@mbilker.us>
2017-08-15 00:44:06 -0400

    polybar: enable bottom bar on DP-1

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,9 +8,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 #polybar -q top &
-#MONITOR=DP-1 polybar -q bottom &
-#MONITOR=DP-3.1 polybar -q multimonitor &
 MONITOR=DP-1 polybar -q bottom &
+#MONITOR=DP-3.1 polybar -q multimonitor &
 MONITOR=DP-3.1 polybar -q bottom &
 MONITOR=DP-3.2 polybar -q bottom &
 MONITOR=DP-3.3 polybar -q multimonitor &
