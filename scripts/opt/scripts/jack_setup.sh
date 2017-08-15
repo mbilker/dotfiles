48f1c0c1d208241495b30e08486f6a5bd5132206
mbilker <me@mbilker.us>
2017-08-15 00:42:10 -0400

    scripts: separate osu jack into separate file

--- scripts/opt/scripts/jack_setup.sh
+++ scripts/opt/scripts/jack_setup.sh
@@ -14,7 +14,9 @@ pacmd set-default-sink jack_out
 # loop client creation
 #/usr/bin/alsa_out -j ploop -dploop -q 1 2>&1 1>/dev/null &
 #/usr/bin/alsa_in -j  cloop -dcloop -q 1 2>&1 1>/dev/null &
-/usr/bin/alsa_in -j osu -dosu_capture -q 1 2>&1 1>/dev/null &
+
+# osu loop client creation
+#/usr/bin/alsa_in -j osu -dosu_capture -q 1 2>&1 1>/dev/null &
 
 # give it some time before connecting to system ports
 sleep 1
@@ -22,8 +24,10 @@ sleep 1
 # cloop ports -> jack output ports
 #jack_connect cloop:capture_1 system:playback_1
 #jack_connect cloop:capture_2 system:playback_2
-jack_connect osu:capture_1 system:playback_1
-jack_connect osu:capture_2 system:playback_2
+
+# osu ports -> jack output ports
+#jack_connect osu:capture_1 system:playback_1
+#jack_connect osu:capture_2 system:playback_2
 
 # done
 exit 0
