a34459a591ea0c38ce498f5d7b88d4b7581bec28
mbilker <me@mbilker.us>
2017-08-23 00:52:51 -0400

    scripts: separate NVIDIA specific config commands before if statements

--- scripts/opt/scripts/dock_helper.sh
+++ scripts/opt/scripts/dock_helper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set -x
+
 FIRST_EXTERNAL="DP-3.3"
 #FIRST_EXTERNAL="DP-3.1"
 FIRST_EXTERNAL_SETTING="1280x1024_60 +0+0"
@@ -11,7 +13,12 @@ PRIMARY_EXTERNAL="${SECOND_EXTERNAL}"
 EXTERNAL_METAMODE="${FIRST_EXTERNAL}: ${FIRST_EXTERNAL_SETTING} { ForceFullCompositionPipeline = On }, ${SECOND_EXTERNAL}: ${SECOND_EXTERNAL_SETTING} { ForceFullCompositionPipeline = On }"
 #EXTERNAL_METAMODE="${FIRST_EXTERNAL}: ${FIRST_EXTERNAL_SETTING} { ForceFullCompositionPipeline = On }"
 
+NVIDIA=0
 if [[ -z "$(/usr/sbin/lspci | grep "HD Graphics")" ]]; then
+  NVIDIA=1
+fi
+
+if [ $NVIDIA -eq 1 ]; then
   LCD_SCREEN="DP-4"
 else
   LCD_SCREEN="eDP-1-1"
@@ -33,8 +40,12 @@ TRACKPOINT_IDS=$(xinput --list | awk -v search="$TRACKPOINT" \
 case "$1" in
   "0")
     # undocked
-    #/usr/bin/nvidia-settings -a CurrentMetaMode="${LCD_SCREEN}: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
-    /usr/bin/xrandr --output "${LCD_SCREEN}" --primary --auto --output "${FIRST_EXTERNAL}" --off --output "${SECOND_EXTERNAL}" --off
+    if [ $NVIDIA -eq 1 ]; then
+      /usr/bin/nvidia-settings -a CurrentMetaMode="${LCD_SCREEN}: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+      /usr/bin/xrandr --output "${LCD_SCREEN}" --primary --output "${FIRST_EXTERNAL}" --off --output "${SECOND_EXTERNAL}" --off
+    else
+      /usr/bin/xrandr --output "${LCD_SCREEN}" --auto --primary --output "${FIRST_EXTERNAL}" --off --output "${SECOND_EXTERNAL}" --off
+    fi
     for id in $TRACKPOINT_IDS; do
       /usr/bin/xinput set-int-prop "${id}" "Device Enabled" 8 1
     done
@@ -44,7 +55,9 @@ case "$1" in
   "1")
     # docked
     /usr/bin/xrandr --output "${LCD_SCREEN}" --off
-    /usr/bin/nvidia-settings -a CurrentMetaMode="${EXTERNAL_METAMODE}"
+    if [ $NVIDIA -eq 1 ]; then
+      /usr/bin/nvidia-settings -a CurrentMetaMode="${EXTERNAL_METAMODE}"
+    fi
     /usr/bin/xrandr --output "${PRIMARY_EXTERNAL}" --primary
     for id in $TRACKPOINT_IDS; do
       /usr/bin/xinput set-int-prop "${id}" "Device Enabled" 8 0
